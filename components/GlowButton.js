9f4b92a7dece8e37bc5235d7883c2ba705c251da
FUTRADERX <asedrew117@gmail.com>
2025-04-17 22:35:47 -0700

    Update GlowButton.js

--- components/GlowButton.js
+++ components/GlowButton.js
@@ -1,1 +1,13 @@
+// GlowButton.js
+import React from 'react';
+import styles from '../styles/GlowButton.module.css';
 
+const GlowButton = ({ children, onClick }) => {
+  return (
+    <button onClick={onClick} className={styles.glowButton}>
+      {children}
+    </button>
+  );
+};
+
+export default GlowButton;
