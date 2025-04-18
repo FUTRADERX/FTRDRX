1615c4f79300c2eced1860f72b0cc338b9110bce
FUTRADERX <asedrew117@gmail.com>
2025-04-17 22:36:49 -0700

    Update AuthGuard.js

--- components/AuthGuard.js
+++ components/AuthGuard.js
@@ -1,1 +1,17 @@
+// components/AuthGuard.js
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
+export default function AuthGuard({ children }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const isAuthenticated = localStorage.getItem('authToken');
+
+    if (!isAuthenticated) {
+      router.push('/login');
+    }
+  }, [router]);
+
+  return <>{children}</>;
+}
