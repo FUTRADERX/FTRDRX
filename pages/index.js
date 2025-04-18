1d4851e3810543c08d3aeb56ce2c7f4903ce799c
FUTRADERX <asedrew117@gmail.com>
2025-04-17 22:28:59 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,21 @@
+import Link from 'next/link';
 
+export default function Home() {
+  return (
+    <div className="landing-container">
+      <img src="/logo.png" alt="FutraderX" className="landing-logo" />
+
+      <h1 className="headline">Trade Like a Funded Pro</h1>
+      <p className="subhead">Pass the combine. Get funded. Keep up to 90% profits.</p>
+
+      <div className="button-group">
+        <Link href="/login">
+          <button className="glow-button">Login</button>
+        </Link>
+        <Link href="/signup">
+          <button className="glow-button alt">Start Challenge</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
